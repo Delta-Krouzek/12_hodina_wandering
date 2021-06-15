d4fc2f0aaa41ae4dd4decd354730500b3641f8c7
czM1K3 <misamadera1@gmail.com>
2021-06-15 16:14:45 +0200

    Hodina

---
+++ Assets/GameInstructorScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInstructorScript : MonoBehaviour
+{
+    public GameObject pandulak;
+    public int numPandulaks = 100;
+
+    public float distanceFromStart = 100;
+    
+    void Start()
+    {
+        for (int i = 0; i < numPandulaks; i++)
+        {
+            Vector3 newPosition = new Vector3(Random.Range(0, distanceFromStart), 0, Random.Range(0, distanceFromStart));
+            Quaternion quaternion = Quaternion.Euler(0, Random.Range(0, 360), 0);
+            Instantiate(pandulak, newPosition, quaternion);
+        }
+    }
+}
---
+++ Assets/WanderScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WanderScript : MonoBehaviour
+{
+    public float speed = 5;
+    public float directionChangeInterval = 1;
+    public float maxHeadingChange = 30;
+
+    private CharacterController controller;
+    private float heading;
+    private Vector3 targetRotation;
+
+    void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+        if (!controller) controller = gameObject.AddComponent<CharacterController>();
+
+        heading = Random.Range(0, 360);
+        transform.eulerAngles = new Vector3(0, heading, 0);
+
+        StartCoroutine(NewHeading());
+    }
+
+    void Update()
+    {
+        transform.eulerAngles =
+            Vector3.Slerp(transform.eulerAngles, targetRotation, Time.deltaTime * directionChangeInterval);
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        controller.SimpleMove(forward * speed);
+    }
+
+    IEnumerator NewHeading()
+    {
+        while (true)
+        {
+            var floor = Mathf.Clamp(heading - maxHeadingChange, 0, 360);
+            var ceil = Mathf.Clamp(heading + maxHeadingChange, 0, 360);
+            heading = Random.Range(floor, ceil);
+            targetRotation = new Vector3(0, heading, 0);
+            yield return new WaitForSeconds(directionChangeInterval);
+        }
+    }
+}
